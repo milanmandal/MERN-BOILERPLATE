a6f2052f509a6715b57c60b0548629841339eda8
milanmandal <51320381+milanmandal@users.noreply.github.com>
2022-05-26 13:14:48 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const {connectDB} = require('./config/db');
+const {auth} = require('./middleware/auth.js');
 
 require('dotenv').config();
 
