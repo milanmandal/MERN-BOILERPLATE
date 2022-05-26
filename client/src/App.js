ff8eff7bd261faf63daff292c83fefb88e3be89f
Milan Mandal <milanmandal2001@gmail.com>
2022-05-27 00:50:12 +0530

    server

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import './App.css';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
